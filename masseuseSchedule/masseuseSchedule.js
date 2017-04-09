00702b519baa31c477ff50970f0c1e07ef21b6c7
bedaria <dburinsk@gmail.com>
2017-04-09 11:44:13 -0700

    Brute force solution done.

---
+++ masseuseSchedule/masseuseSchedule.js
@@ -0,0 +1,34 @@
+/*A popular masseuse receives a sequence of back-to-back appointment requests
+and is debating which ones to accept. She needs a 15-minute break between
+appointments and therefore she cannot accept any adjacent requests. Given a
+sequence of back-to-back appointment requests (all multiples of 15 minutes, none
+overlap, and none can be moved), find the optimal (highest total booked minutes)
+set the masseuse can honor. Return the number of minutes.
+
+INPUT: [30,15,60,75,45,15,15,45]
+OUTPUT: 180 minutes, ([30, 60, 45, 45])
+*/
+
+function findOptimalSchedule(array) {
+  if(!array.length)
+    return 0
+
+  var bestTotal = 0
+  var bestAppointments = []
+
+  function findBest(currentIdx, total) {
+    if(currentIdx >= array.length - 2)
+      if(total > bestTotal)
+        bestTotal = total
+
+    for(var i = currentIdx + 2; i < array.length; i++)
+      findBest(i, total + array[i])
+  }
+
+  for(var i = 0; i < array.length; i++)
+    findBest(i, array[i])
+
+  return bestTotal
+}
+
+module.exports = findOptimalSchedule
---
+++ masseuseSchedule/test/masseuseTest.js
@@ -0,0 +1,43 @@
+const chai = require('chai')
+const expect = chai.expect
+const findOptimalSchedule = require('../masseuseSchedule.js')
+
+describe("function mergeSynonyms", () => {
+
+  it("Works when schedule is []", () => {
+    expect(findOptimalSchedule([])).to.be.empty
+  })
+
+  it("For [30,15] returns 30", () => {
+    expect(findOptimalSchedule([30,15])).to.be.eql(30)
+  })
+
+  it("For [30,15,45] returns 75", () => {
+    expect(findOptimalSchedule([30,15,45])).to.be.eql(75)
+  })
+
+  it("For [15,45,30] returns 45", () => {
+    expect(findOptimalSchedule([15,45,30])).to.be.eql(45)
+  })
+
+  it("For [15,60,35] returns 60", () => {
+    expect(findOptimalSchedule([15,60,35])).to.be.eql(60)
+  })
+
+  it("For [15,15,60,30] returns 75", () => {
+    expect(findOptimalSchedule([15,15,60,30])).to.be.eql(75)
+  })
+
+  it("For [15,15,15,15] returns 30", () => {
+    expect(findOptimalSchedule([15,15,15,15])).to.be.eql(30)
+  })
+
+  it("For [15,15,15,15,15] returns 45", () => {
+    expect(findOptimalSchedule([15,15,15,15,15])).to.be.eql(45)
+  }) 
+
+  it("For [30,15,60,75,45,15,15,45] returns 180", () => {
+    expect(findOptimalSchedule([30,15,60,75,45,15,15,45])).to.be.eql(180)
+  })
+
+})
